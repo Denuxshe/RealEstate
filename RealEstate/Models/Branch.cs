f7740b4f2070fc02e9e4e92279374ed806895243
Denu <Denu@DENUXSHE>
2020-10-16 20:37:14 +0530

    created mdel

---
+++ RealEstate/Models/Branch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RealEstate.Models
+{
+    public class Branch
+    {
+        public int branchNo { get; set; }
+        public string Street { get; set; }
+        public string city { get; set; }
+        public int postcode { get; set; }
+
+    }
+}
