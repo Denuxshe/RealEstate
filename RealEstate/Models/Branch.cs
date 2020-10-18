9da314b4b888c6fa70bd696912fa288e7de43753
Denu <Denu@DENUXSHE>
2020-10-18 09:48:30 +0530

    UpdateTable

--- RealEstate/Models/Branch.cs
+++ RealEstate/Models/Branch.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace RealEstate.Models
 {
+    [Table("Branch_tbl")]
     public class Branch
     {
+        [Key]
         public int branchNo { get; set; }
         public string Street { get; set; }
         public string city { get; set; }
--- RealEstate/Models/Staff.cs
+++ RealEstate/Models/Staff.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace RealEstate.Models
 {
+    [Table("Staff_tbl")]
     public class Staff
     {
+        [Key]
+        public int staffNo { get; set; }
+        public string fName{ get; set; }
+        public string lName { get; set; }
+        public string position { get; set; }
+        public DateTime dateOfBirth { get; set; }
+        public int salary { get; set; }
+
+        [ForeignKey("branch")]        
+        public int branchNo { get; set; }
+        public Branch branch { get; set; }
+
     }
 }
